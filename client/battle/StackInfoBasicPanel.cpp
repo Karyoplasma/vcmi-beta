96776f698e7bb33026f384e12389c7b965cad027
Karyoplasma <karyoplasma@proton.me>
2026-08-04 21:09:18 +0200

    Update StackInfoBasicPanel.cpp

--- client/battle/StackInfoBasicPanel.cpp
+++ client/battle/StackInfoBasicPanel.cpp
@@ -47,10 +47,8 @@ void StackInfoBasicPanel::initializeData(const CStack * stack)
 	int damageMultiplier = 1;
 	if (stack->hasBonusOfType(BonusType::SIEGE_WEAPON))
 	{
-		damageMultiplier = LIBRARY->creatures()->getByIndex(stack->creatureIndex())->getWarMachineOffset();
-		if (damageMultiplier < 2){
-			logGlobal->info("War machine offset is less than 2 for Siege Weapon.");
-		}
+		damageMultiplier = stack->getWarMachineOffset();
+		logGlobal->info("damageMultiplier = %d", damageMultiplier);
 		static const auto bonusSelector =
 			Selector::sourceTypeSel(BonusSource::ARTIFACT).Or(
 															  Selector::sourceTypeSel(BonusSource::HERO_BASE_SKILL)).And(
